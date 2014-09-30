4f1dc70630b0163a20c6bab4a3102f8553dd7aa7
misterkak <mike.kakebeeke@gmail.com>
2014-09-30 16:18:17 -0700

    first commit of 'upload images'

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,16 @@
 class Movie < ActiveRecord::Base
 
   has_many :reviews
+  mount_uploader :image, ImageUploader
 
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validates :description, presence: true
-  validates :poster_image_url, presence: true
+  validates :image, presence: true
   validates :release_date, presence: true
   validate :release_date_is_in_the_future # NOTE this is a custom validation
+
 
   def review_average
     if reviews.size != 0
