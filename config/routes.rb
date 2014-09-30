96d3119beae8130cec4e2061f61c173705b990ae
misterkak <mike.kakebeeke@gmail.com>
2014-09-30 11:10:52 -0700

    update for login and flash hash

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
   resources :movies
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
   
+  
+
   # get 'movies/index'
   # get 'movies/show'
   # get 'movies/new'
